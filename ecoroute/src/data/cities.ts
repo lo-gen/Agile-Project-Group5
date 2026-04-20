52c910afe3f778e8a657bbbc4ee3fd825c16447b
kasi <ia125204@gmail.com>
2026-04-20 15:04:19 +0200

    feat: add 20 European cities and transport mode definitions

---
+++ ecoroute/src/data/cities.ts
@@ -0,0 +1,24 @@
+import type { City } from '../types'
+
+export const cities: City[] = [
+  { id: 'arn', name: 'Stockholm',   country: 'Sweden',          iata: 'ARN', lat: 59.651901,  lng: 17.918600  },
+  { id: 'cph', name: 'Copenhagen',  country: 'Denmark',         iata: 'CPH', lat: 55.617900,  lng: 12.656000  },
+  { id: 'osl', name: 'Oslo',        country: 'Norway',          iata: 'OSL', lat: 60.193900,  lng: 11.100400  },
+  { id: 'hel', name: 'Helsinki',    country: 'Finland',         iata: 'HEL', lat: 60.317200,  lng: 24.963301  },
+  { id: 'lhr', name: 'London',      country: 'United Kingdom',  iata: 'LHR', lat: 51.477500,  lng: -0.461941  },
+  { id: 'ams', name: 'Amsterdam',   country: 'Netherlands',     iata: 'AMS', lat: 52.308601,  lng: 4.763890   },
+  { id: 'bru', name: 'Brussels',    country: 'Belgium',         iata: 'BRU', lat: 50.901402,  lng: 4.484440   },
+  { id: 'cdg', name: 'Paris',       country: 'France',          iata: 'CDG', lat: 49.012798,  lng: 2.550000   },
+  { id: 'txl', name: 'Berlin',      country: 'Germany',         iata: 'BER', lat: 52.351399,  lng: 13.493400  },
+  { id: 'muc', name: 'Munich',      country: 'Germany',         iata: 'MUC', lat: 48.353802,  lng: 11.786100  },
+  { id: 'waw', name: 'Warsaw',      country: 'Poland',          iata: 'WAW', lat: 52.165798,  lng: 20.967098  },
+  { id: 'vie', name: 'Vienna',      country: 'Austria',         iata: 'VIE', lat: 48.110298,  lng: 16.569700  },
+  { id: 'prg', name: 'Prague',      country: 'Czech Republic',  iata: 'PRG', lat: 50.100700,  lng: 14.260000  },
+  { id: 'zrh', name: 'Zurich',      country: 'Switzerland',     iata: 'ZRH', lat: 47.464699,  lng: 8.549170   },
+  { id: 'mad', name: 'Madrid',      country: 'Spain',           iata: 'MAD', lat: 40.471926,  lng: -3.560264  },
+  { id: 'bcn', name: 'Barcelona',   country: 'Spain',           iata: 'BCN', lat: 41.297100,  lng: 2.078460   },
+  { id: 'lis', name: 'Lisbon',      country: 'Portugal',        iata: 'LIS', lat: 38.781300,  lng: -9.135920  },
+  { id: 'fco', name: 'Rome',        country: 'Italy',           iata: 'FCO', lat: 41.800301,  lng: 12.238900  },
+  { id: 'mxp', name: 'Milan',       country: 'Italy',           iata: 'MXP', lat: 45.630601,  lng: 8.723110   },
+  { id: 'ath', name: 'Athens',      country: 'Greece',          iata: 'ATH', lat: 37.936401,  lng: 23.944500  },
+]
