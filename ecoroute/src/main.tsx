8a0ad34e7baf0d52e81b859e40497ec99aa2b494
kasi <ia125204@gmail.com>
2026-04-25 20:27:53 +0200

    feat: wrap app with AuthProvider

--- ecoroute/src/main.tsx
+++ ecoroute/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { FlightProvider } from './context/FlightContext'
+import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FlightProvider>
-      <App />
-    </FlightProvider>
+    <AuthProvider>
+      <FlightProvider>
+        <App />
+      </FlightProvider>
+    </AuthProvider>
   </StrictMode>,
 )
